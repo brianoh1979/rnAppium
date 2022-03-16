10e055e455d63f609df0b7a8255afa1d86799d11
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-03-16 18:20:23 +0000

    Changing test file to be one from other project

--- test/specs/App2.test4.js
+++ test/specs/App2.test4.js
@@ -1,19 +1,9 @@
-function filterByTerm(inputArr, searchTerm) {
-  return inputArr.filter(function(arrayElement) {
-    return arrayElement.url.match(searchTerm);
-  });
-}
+import React from 'react';
+import App from './App';
 
-describe("Filter function", () => {
-  test("it should filter by a search term (link)", () => {
-    const input = [
-      { id: 1, url: "https://www.url1.dev" },
-      { id: 2, url: "https://www.url2.dev" },
-      { id: 3, url: "https://www.link3.dev" }
-    ];
+import renderer from 'react-test-renderer';
 
-    const output = [{ id: 3, url: "https://www.link3.dev" }];
-
-    expect(filterByTerm(input, "link")).toEqual(output);
-  });
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
 });
